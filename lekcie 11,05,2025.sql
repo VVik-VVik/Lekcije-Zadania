eaf6761eb20b990cdf8a6ea8bbb53da1aa0e63df
VVik-VVik <miriadinna@gmail.com>
2025-05-11 10:00:39 +0200

    Update lekcie 11,05,2025.sql

--- lekcie 11,05,2025.sql
+++ lekcie 11,05,2025.sql
@@ -6,8 +6,13 @@ inf_03_2024_06_09_SG.pdf
 
 inf_03_2024_06_10_SG.pdf
 1. SELECT COUNT(*), pencja FROM pracownicy GROUP BY pensja ORDER BY pencja DESC;
-//
-2. SELECT imie, nazwisko
+// SELECT COUNT(*) AS liczba_pracownikow, GROUP_CONCAT(nazwisko SEPARATOR ', ') AS nazwisko, pencja FROM pracownicy GROUP BY pensja ORDER BY pensja DESC;
+2. SELECT pracownicy.imie, pracownicy.nazwisko, stanowiska.nazwa FROM pracownicy JOIN stanowiska ON pracownicy.stanowiska_id=stanowiska.id WHERE staz > 10;
+3. SELECT nazwisko, pencja FROM pracownicy WHERE staz BETWEEN 10 AND 20;
+4. ALTER TABLE stanowiska ADD minPensja int;
+
+inf_03_2024_06_11_SG.pdf
+1. SELECT imie, miot FROM swinki
+2.
 3.
 4.
-
